cc08312193f7032a16b401ff635e56304a281aca
Tetteh Kephas Teye-Lao <tettehsaintluke@gmail.com>
2025-04-12 16:35:20 +0000

    Update getData.js

--- server/utils/getData.js
+++ server/utils/getData.js
@@ -1,19 +1,18 @@
-const getData = async (req, res, next, url, page) => {
+const getData = async (req, res) => {
     try {
         const response = await fetch(`${url}&api_key=${process.env.API_KEY}${page ? '&page=' + page : ''}`);
         const data = await response.json();
 
-        if (!data || !data.results) {
+        if (!data) {
             return res.status(500).json({ message: "Invalid data received from API." });
         }
 
-        return res.json(data.results);
+        return res.json({ debug: true, fullData: data });
     } catch (err) {
         console.error("Fetch error:", err.message || err);
         return res.status(500).json({ message: "Something went wrong, refresh the page." });
     }
 };
-
 
 module.exports ={
     getData
