b39c113e09029c442514829f10df70a2e5426398
Tetteh Kephas Teye-Lao <tettehsaintluke@gmail.com>
2025-04-12 17:00:21 +0000

    Update getData.js

--- server/utils/getData.js
+++ server/utils/getData.js
@@ -4,9 +4,9 @@ const getData = async (req, res, next, url, page) => {
         const response = await fetch(`${url}&api_key=${process.env.API_KEY}${page ? '&page=' + page : ''}`);
         const data = await response.json();
 
-        console.log("RAW DATA FROM API:", data); 
-
+        
         if (!data || !data.results) {
+            console.log("RAW DATA FROM API:", data); 
             return res.status(500).json({ message: "Invalid data received from API." });
         }
 
